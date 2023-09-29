3baaa5fac037bb05857ad8066ed0d3e3a4824ce0
kr-younghoon <sullanta0802@gmail.com>
2023-09-29 23:20:39 +0900

    Feat: 필수 기능들을 위한 html 코드 구현

--- main/src/Home/Step3Form.js
+++ main/src/Home/Step3Form.js
@@ -1,9 +1,16 @@
+import PersonTraitsSelector from './Components/Input/PersonTraitsSelector';
+
 const Step3Form = () => {
     // Step 3의 폼 컴포넌트 내용을 구현
     return (
         <div>
-            <h2>Step 3</h2>
-            {/* 폼 요소들을 추가 */}
+            <h1>
+                "name" 청년은
+                <br />
+                어떤 사람인가요?
+            </h1>
+            <h5>어울리는 키워드를 모두 골라주세요!</h5>
+            <PersonTraitsSelector />
         </div>
     );
 };
