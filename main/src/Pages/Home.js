9683861fbd76bb038a34d7d2c7ce532ebfb50ff2
kr-younghoon <sullanta0802@gmail.com>
2023-10-03 20:13:32 +0900

    Edit: 메인 마지막 페이지 추가/전 페이지 등록 완료

--- main/src/Pages/Home.js
+++ main/src/Pages/Home.js
@@ -3,6 +3,10 @@ import Step1Form from '../Home/Step1Form';
 import Step2Form from '../Home/Step2Form';
 import Step3Form from '../Home/Step3Form';
 import Step4Form from '../Home/Step4Form';
+import Step5Form from '../Home/Step5Form';
+import Step6Form from '../Home/Step6Form';
+import Step7Form from '../Home/Step7Form';
+import Step8Form from '../Home/Step8Form';
 
 const Home = () => {
     const [step, setStep] = useState(1);
@@ -21,10 +25,14 @@ const Home = () => {
             {step === 2 && <Step2Form />}
             {step === 3 && <Step3Form />}
             {step === 4 && <Step4Form />}
+            {step === 5 && <Step5Form />}
+            {step === 6 && <Step6Form />}
+            {step === 7 && <Step7Form />}
+            {step === 8 && <Step8Form />}
 
             <div>
                 {step > 1 && <button onClick={prevStep}>이전 단계</button>}
-                {step < 4 && <button onClick={nextStep}>다음 단계</button>}
+                {step < 8 && <button onClick={nextStep}>다음 단계</button>}
             </div>
         </div>
     );
