0efdfc61f682922b608022518d46b89a14d2362c
kr-younghoon <sullanta0802@gmail.com>
2023-10-03 20:11:16 +0900

    Feat: 시간과 장소 안내 페이지

---
+++ main/src/Pages/PlacesAndTimes.js
@@ -0,0 +1,34 @@
+const PlacesAndTimes = () => {
+    return (
+        <>
+            <h1>
+                이날 예배는 이렇게 <br />
+                진행되어요.
+            </h1>
+            <div>
+                <div>
+                    <h5>16:00-16:40</h5>
+                    <h1>예배 시작 및 찬양</h1>
+                </div>
+                <div>
+                    <h5>16:40-17:20</h5>
+                    <h1>
+                        설교말씀
+                        <br />
+                        (Guest. Jeon Eunju)
+                    </h1>
+                </div>
+                <div>
+                    <h5>17:20-17:40</h5>
+                    <h1>헌금과 기도</h1>
+                </div>
+            </div>
+            <h1>우리 교회는 여기예요.</h1>
+            <p>지도 정보</p>
+            <h3>인천광역시 중구 신도시북로 74</h3>
+            <button>네이버 지도로 열기</button>
+        </>
+    );
+};
+
+export default PlacesAndTimes;
