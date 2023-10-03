eee5d5605dabf8eecfb49edac4f18519303a3cc8
kr-younghoon <sullanta0802@gmail.com>
2023-10-03 16:31:21 +0900

    Feat: 페이지7

---
+++ main/src/Home/Step7Form.js
@@ -0,0 +1,19 @@
+const Step7Form = () => {
+    return (
+        <>
+            <h1>
+                마지막으로,
+                <br />
+                | Name |님을
+                <br />
+                축복하기 위해
+                <br />
+                성경 말씀 한 구절
+                <br />
+                준비했어요!
+            </h1>
+        </>
+    );
+};
+
+export default Step7Form;
