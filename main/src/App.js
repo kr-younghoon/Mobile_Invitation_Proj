5134e1e89c5ea0b2c239adcac5a098741f4aebdb
kr-younghoon <sullanta0802@gmail.com>
2023-10-03 20:12:25 +0900

    Feat: 방명록 페이지

--- main/src/App.js
+++ main/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Home from './Pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PlacesAndTimes from './Pages/PlacesAndTimes';
+import Reviews from './Pages/Reviews';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/home" element={<Home />} />
-                {/* <Route path="/places-and-times" element={<MultiStepForm />} /> */}
-                {/* <Route path="/reviews" element={<MultiStepForm />} /> */}
+                <Route path="/places-and-times" element={<PlacesAndTimes />} />
+                <Route path="/reviews" element={<Reviews />} />
             </Routes>
         </Router>
     );
