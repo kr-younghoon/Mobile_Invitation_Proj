f6627cae6f107488d80e4b8e2c65dee2b7df1e13
kr-younghoon <sullanta0802@gmail.com>
2023-10-03 22:04:09 +0900

    Edit: 로케이션 수정, Lottie JSON 관련 코드 추가

---
+++ main/src/Home/Form/Step7Form.js
@@ -0,0 +1,25 @@
+// import Lottie from 'lottie-react';
+// import BibleIconData from '../Assets/bible.json';
+
+// import BibleIcon from '../Assets/BibleIcon';
+
+const Step7Form = () => {
+    return (
+        <>
+            <h1>
+                마지막으로,
+                <br />
+                | Name |님을
+                <br />
+                축복하기 위해
+                <br />
+                성경 말씀 한 구절
+                <br />
+                준비했어요!
+            </h1>
+            {/* <BibleIcon /> */}
+        </>
+    );
+};
+
+export default Step7Form;
