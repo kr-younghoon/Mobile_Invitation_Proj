29dc9a194e7f4458b9a1d18bc1afbdfd7163f675
kr-younghoon <sullanta0802@gmail.com>
2023-10-10 12:31:49 +0900

    Feat: PlacesAndTimes CSS 구현 #3

--- src/Pages/PlacesAndTimes.jsx
+++ src/Pages/PlacesAndTimes.jsx
@@ -1,111 +1,48 @@
+import MapComponent from '../Home/Components/Map/MapComponent';
+import styles from './PlacesAndTimes.module.css';
+
 function PlacesAndTimes() {
     return (
-        <>
-            <h1>
+        <div className={styles.scrollable_div}>
+            <p className={styles.title}>
                 이날 예배는 이렇게 <br />
                 진행되어요.
-            </h1>
+            </p>
             <div>
-                <div>
-                    <h5>16:00-16:40</h5>
-                    <h1>예배 시작 및 찬양</h1>
+                <div className={styles.sub}>
+                    <p className={styles.sub_cap}>16:00-16:40</p>
+                    <p className={styles.sub_lg}>예배 시작 및 찬양</p>
                 </div>
-                <div>
-                    <h5>16:40-17:20</h5>
-                    <h1>
+                <div className={styles.sub}>
+                    <p className={styles.sub_cap}>16:40-17:20</p>
+                    <p className={styles.sub_lg}>
                         설교말씀
                         <br />
                         (Guest. Jeon Eunju)
-                    </h1>
+                    </p>
                 </div>
-                <div>
-                    <h5>17:20-17:40</h5>
-                    <h1>헌금과 기도</h1>
-                </div>
-            </div>
-            <h1>우리 교회는 여기예요.</h1>
-            <p>지도 정보</p>
-            <h3>인천광역시 중구 신도시북로 74</h3>
-            <button>네이버 지도로 열기</button>
-            <h1>
-                이날 예배는 이렇게 <br />
-                진행되어요.
-            </h1>
-            <div>
-                <div>
-                    <h5>16:00-16:40</h5>
-                    <h1>예배 시작 및 찬양</h1>
-                </div>
-                <div>
-                    <h5>16:40-17:20</h5>
-                    <h1>
-                        설교말씀
-                        <br />
-                        (Guest. Jeon Eunju)
-                    </h1>
-                </div>
-                <div>
-                    <h5>17:20-17:40</h5>
-                    <h1>헌금과 기도</h1>
+                <div className={styles.sub}>
+                    <p className={styles.sub_cap}>17:20-17:40</p>
+                    <p className={styles.sub_lg}>헌금과 기도</p>
                 </div>
             </div>
-            <h1>우리 교회는 여기예요.</h1>
-            <p>지도 정보</p>
-            <h3>인천광역시 중구 신도시북로 74</h3>
-            <button>네이버 지도로 열기</button>
-            <h1>
-                이날 예배는 이렇게 <br />
-                진행되어요.
-            </h1>
-            <div>
-                <div>
-                    <h5>16:00-16:40</h5>
-                    <h1>예배 시작 및 찬양</h1>
-                </div>
-                <div>
-                    <h5>16:40-17:20</h5>
-                    <h1>
-                        설교말씀
-                        <br />
-                        (Guest. Jeon Eunju)
-                    </h1>
-                </div>
-                <div>
-                    <h5>17:20-17:40</h5>
-                    <h1>헌금과 기도</h1>
-                </div>
+            <p className={styles.title2}>우리 교회는 여기예요.</p>
+            <div className={styles.map}>
+                <MapComponent />
             </div>
-            <h1>우리 교회는 여기예요.</h1>
-            <p>지도 정보</p>
-            <h3>인천광역시 중구 신도시북로 74</h3>
-            <button>네이버 지도로 열기</button>
-            <h1>
-                이날 예배는 이렇게 <br />
-                진행되어요.
-            </h1>
-            <div>
-                <div>
-                    <h5>16:00-16:40</h5>
-                    <h1>예배 시작 및 찬양</h1>
-                </div>
-                <div>
-                    <h5>16:40-17:20</h5>
-                    <h1>
-                        설교말씀
-                        <br />
-                        (Guest. Jeon Eunju)
-                    </h1>
-                </div>
-                <div>
-                    <h5>17:20-17:40</h5>
-                    <h1>헌금과 기도</h1>
-                </div>
+            <p className={styles.sub_sm}>인천광역시 중구 신도시북로 74</p>
+            <div className={styles.openMapBtn_layout}>
+                <a
+                    href="https://naver.me/GsTXzEG4"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <button className={styles.openMapBtn}>
+                        네이버 지도로 열기
+                    </button>
+                </a>
             </div>
-            <h1>우리 교회는 여기예요.</h1>
-            <p>지도 정보</p>
-            <h3>인천광역시 중구 신도시북로 74</h3>
-            <button>네이버 지도로 열기</button>
-        </>
+        </div>
     );
 }
 
