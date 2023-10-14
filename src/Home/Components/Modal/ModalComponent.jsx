d62b69db31d3bcf629330ef0101cfa019f156ee3
kr-younghoon <sullanta0802@gmail.com>
2023-10-15 04:15:18 +0900

    [#4] Edit: 7페이지 CSS 완성 및 수정

--- src/Home/Components/Modal/ModalComponent.jsx
+++ src/Home/Components/Modal/ModalComponent.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import Modal from 'react-modal';
 import styles from './ModalComponent.module.css';
 import ModalCloseBtn from '../../Assets/SVG/ModalCloseBtn';
@@ -7,25 +9,43 @@ import DUMMY from '../../Assets/JSON/heart-boom.json';
 function ModalComponent({ isOpen, closeModal, content }) {
     const ModalStyle = {
         overlay: {
-            position: 'absolute',
+            position: 'fixed',
+            display: 'flex',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            margin: 'auto',
+            // zIndex: 1000,
+            justifyContent: 'center',
+            alignItems: 'center',
         },
         content: {
-            display: 'flex',
-            flexDirection: 'column',
+            position: 'relative',
+            inset: 0,
             overflow: 'auto',
             width: '320px',
             height: '550px',
-            top: '0px',
-            left: '0px',
-            right: '0px',
-            bottom: '0px',
-            margin: 'auto',
             borderRadius: '20px',
-            zIndex: 1000,
             padding: 0,
-            // padding: '30px',
         },
     };
+
+    const [showLottie, setShowLottie] = useState(true);
+    useEffect(() => {
+        let timer;
+
+        if (isOpen) {
+            timer = setTimeout(() => {
+                setShowLottie(false); // 5초 후 Lottie를 숨김
+            }, 5000); // 5000 밀리초 (5초)
+        }
+
+        return () => {
+            clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 해제
+            setShowLottie(true);
+        };
+    }, [isOpen]);
 
     return (
         <Modal
@@ -35,22 +55,24 @@ function ModalComponent({ isOpen, closeModal, content }) {
             isOpen={isOpen}
             onRequestClose={closeModal}
         >
-            <div className={styles.content}>
-                <Lottie
-                    animationData={DUMMY}
-                    // loop={false}
-                ></Lottie>
+            <div className={styles.modal}>
+                <div
+                    className={styles.ModalLottie}
+                    style={{ display: showLottie ? 'block' : 'none' }}
+                >
+                    <Lottie animationData={DUMMY} loop={false}></Lottie>
+                </div>
                 <div className={`${styles.layout} ${styles.slide_in2}`}>
                     <button
                         onClick={closeModal}
-                        className={styles.modalclosebtn}
+                        className={styles.ModalCloseBtn}
                     >
                         <ModalCloseBtn />
                     </button>
-                    <div className={styles.imgContent}>{content}</div>
-                    <p className={`${styles.sub_cap} ${styles.comment}`}>
+                    <p className={`${styles.sub_cap} ${styles.ModalGuideText}`}>
                         길게 탭하여 말씀카드를 저장해보세요!
                     </p>
+                    <div className={styles.ModalImg}>{content}</div>
                 </div>
             </div>
         </Modal>
